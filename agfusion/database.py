56b15111f7473c50f5e5fcb862962a66e9b5ddf5
murphycj <murphy.charlesj@gmail.com>
2016-08-21 22:38:40 -0400

    will use pyensembl instead

--- agfusion/database.py
+++ agfusion/database.py
@@ -228,132 +228,6 @@ class AGFusionDBBManager(AGFusionDB):
 
         return return_dict
 
-    def _fetch_gene_level_info(self,genes,p,table):
-
-        print 'Fetching gene-level information...'
-
-        data = self._fetch(
-            ids=genes,
-            filters='ensembl_gene_id',
-            attributes=[
-                'ensembl_gene_id',
-                'entrezgene',
-                'hgnc_symbol',
-                'chromosome_name',
-                'strand',
-                'start_position',
-                'end_position'
-            ],
-            p=p,
-            batch_size=100
-        )
-
-        #process data
-
-        print 'Adding gene-level information to the database...'
-
-        #format the data correctly so it can be put into the database
-
-        data_into_db = []
-        for index, chunk in data.items():
-            for r in chunk:
-                data_into_db.append([
-                    str(r[0]),
-                    str(r[1]),
-                    str(r[2]),
-                    str(r[3]),
-                    int(r[4]),
-                    int(r[5]),
-                    int(r[6]),
-                ])
-
-        self.c.execute('DELETE FROM ' + table)
-        self.conn.commit()
-
-        self.c.executemany('INSERT INTO ' + table + ' VALUES (?,?,?,?,?,?,?)', data_into_db)
-        self.conn.commit()
-
-    def _fetch_transcript_level_info(self,genes,p):
-
-        print 'Fetching transcript-level information...'
-
-        data = self._fetch(
-            ids=genes,
-            filters='ensembl_gene_id',
-            attributes=[
-                'ensembl_gene_id',
-                'ensembl_transcript_id',
-                'ensembl_peptide_id',
-                'ensembl_exon_id',
-                'transcript_biotype',
-                'transcript_start',
-                'transcript_end',
-                'exon_chrom_start',
-                'exon_chrom_end',
-                'genomic_coding_start',
-                'genomic_coding_end'
-            ],
-            p=p,
-            batch_size=100
-        )
-
-        #process data
-
-        print 'Adding transcript-level information to the database...'
-
-        #insert into transcipt table
-
-        #format the data correctly so it can be put into the database
-
-        data_into_db = []
-        for index, chunk in data.items():
-            for r in chunk:
-                temp = [
-                    str(r[0]),
-                    str(r[1]),
-                    str(r[2]),
-                    str(r[4]),
-                    -1 if r[5]==u'' else int(r[5]),
-                    -1 if r[6]==u'' else int(r[6]),
-                ]
-                if temp not in data_into_db:
-                    data_into_db.append(temp)
-
-        self.c.execute('DELETE FROM ' + self.reference + '_transcript')
-        self.conn.commit()
-
-        self.c.executemany(
-            'INSERT INTO ' + self.reference + '_transcript VALUES (?,?,?,?,?,?)',
-            data_into_db
-        )
-        self.conn.commit()
-
-        #insert into transcipt_annotation table
-
-        #format the data correctly so it can be put into the database
-
-        data_into_db = []
-        for index, chunk in data.items():
-            for r in chunk:
-                data_into_db.append([
-                    str(r[1]),
-                    str(r[3]),
-                    -1 if r[7]==u'' else int(r[7]),
-                    -1 if r[8]==u'' else int(r[8]),
-                    -1 if r[9]==u'' else int(r[9]),
-                    -1 if r[10]==u'' else int(r[10]),
-                ])
-
-        self.c.execute('DELETE FROM ' + self.reference + '_annotation_transcript')
-        self.conn.commit()
-
-        self.c.executemany(
-            'INSERT INTO ' + self.reference + '_annotation_transcript VALUES (?,?,?,?,?,?)',
-            data_into_db
-        )
-        self.conn.commit()
-
-
     def _fetch_protein_level_info(self,genes,p,table):
         print 'Fetching protein-level information...'
 
@@ -432,6 +306,60 @@ class AGFusionDBBManager(AGFusionDB):
         #    self.reference
         #)
 
+    def _parse_gtf(self):
+        """
+        add the GTF file to the SQLite3 database
+        """
+
+        gtf_file = os.path.join(self.database_dir,self.reference,self.reference + '.gtf')
+
+        fin = open(gtf_file,'r')
+
+        data = {'transcript':{},'exons':{}}
+        reverse_strand = False
+
+        for line in fin.readlines():
+
+            #skip the header lines
+
+            if len(re.findall('^#!',line))==0:
+
+                #only get the lines corresponding to the transcript of interest
+
+                if len(re.findall(args.transcript,line))!=0:
+                    line = line.rstrip().split('\t')
+
+                    if line[2]=='transcript':  # get the transcript info
+                        data['transcript'] = {'start': int(line[3]), 'end': int(line[4])}
+
+                        #is it on the reverse strand?
+
+                        if line[6]=='-':
+                            reverse_strand = True
+
+                    elif line[2]=='exon': #exon info
+                        num = re.findall(r'exon_number "\d{1,2}"',line[8])
+                        assert len(num)!=0, 'no exon number found for exon'
+                        num = int(num[0].split('"')[1])
+
+                        data['exons'][num] = {'start':int(line[3]),'end':int(line[4]),'CDS':None}
+
+                    elif line[2]=='CDS': #CDS info
+                        num = re.findall(r'exon_number "\d{1,2}"',line[8])
+
+                        assert len(num)!=0, 'no exon number found for CDS'
+                        num = int(num[0].split('"')[1])
+                        if num not in data['exons']:
+                            import pdb; pdb.set_trace()
+
+                        assert num in data['exons'], 'exon number for CDS not in data'
+
+                        data['exons'][num]['CDS'] = {'start':int(line[3]),'end':int(line[4])}
+
+                        assert data['exons'][num]['CDS']['start'] >= data['exons'][num]['start'], 'CDS starts before exon'
+                        assert data['exons'][num]['CDS']['end'] <= data['exons'][num]['end'], 'CDS starts before exon'
+        fin.close()
+
     def add_fasta_gtf(self):
         """
         Locate the fasta and gtf files and add the path to them to the database
@@ -445,6 +373,9 @@ class AGFusionDBBManager(AGFusionDB):
 
             assert os.path.exists(r), "No files or too many reference files were found %s" % r
 
+            if os.path.splitext(r)[1]=='.fa':
+                assert os.path.exists(r + '.fai'), "No .fai found for %s" % r
+
             files.append(r)
 
         self.c.execute(
@@ -452,3 +383,5 @@ class AGFusionDBBManager(AGFusionDB):
             files
         )
         self.conn.commit()
+
+        self.add_fasta_gtf()
