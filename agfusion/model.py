fb2c2d364e90f2d3d36464f834385ca0d7e09d75
murphycj <murphy.charlesj@gmail.com>
2016-08-27 16:26:40 -0400

    annotate fusion prototype code, and saving data

--- agfusion/model.py
+++ agfusion/model.py
@@ -32,6 +32,61 @@ class Model(object):
     def save_image(self,file_prefix):
         pass
 
+    def save_transcript_cdna(self,out_file):
+
+        fout = open(out_file,'w')
+
+        for name, transcript in self.transcripts.items():
+
+            if transcript.cds is not None:
+
+                SeqIO.write(transcript.cdna,fout,"fasta")
+
+        fout.close()
+
+    def save_transcript_cds(self,out_file):
+
+        fout = open(out_file,'w')
+
+        for name, transcript in self.transcripts.items():
+
+            if transcript.cds is not None:
+
+                SeqIO.write(transcript.cds,fout,"fasta")
+
+        fout.close()
+
+    def save_proteins(self,out_file):
+
+        fout = open(out_file,'w')
+
+        for name, transcript in self.transcripts.items():
+
+            if transcript.cds is not None:
+
+                SeqIO.write(transcript.protein,fout,"fasta")
+
+        fout.close()
+
+    def save_annotations(self,out_file,annotation='pfam'):
+
+        fout = open(out_file,'w')
+        fout.write("Gene,transcript,domain,protein_start,protein_end\n")
+
+        for name, transcript in self.transcripts.items():
+            for domain in transcript.domains[annotation]:
+                try:
+                    fout.write(
+                        self.name + ',' + \
+                        name + ',' + \
+                        domain[1] + ',' + \
+                        str(domain[2]) + ',' + \
+                        str(domain[3]) + '\n'
+                    )
+                except:
+                    import pdb; pdb.set_trace()
+        fout.close()
+
 class Gene(Model):
     """
     Stores the necessary information to specify the architecture of either
@@ -56,6 +111,7 @@ class Fusion(Model):
     def __init__(self,gene5prime=None,gene3prime=None,db=None):
         self.gene5prime=gene5prime
         self.gene3prime=gene3prime
+        self.name = self.gene5prime.gene.name + '-' + self.gene3prime.gene.name
         self.db=db
 
         self.transcripts = {}
@@ -65,7 +121,16 @@ class Fusion(Model):
                         self.gene3prime.gene.transcripts)):
             transcript1 = combo[0]
             transcript2 = combo[1]
-            name=transcript1.id+'-'+transcript2.id
+
+            #skip if the junction is outside the range of either transcript
+
+            if not transcript1.contains(transcript1.contig,self.gene5prime.junction,self.gene5prime.junction):
+                continue
+
+            if not transcript2.contains(transcript2.contig,self.gene3prime.junction,self.gene3prime.junction):
+                continue
+
+            name = transcript1.id+'-'+transcript2.id
             self.transcripts[name] = FusionTranscript(
                 transcript1,
                 transcript2,
@@ -74,58 +139,10 @@ class Fusion(Model):
                 db=db
             )
 
-    def save_transcript_cdna(self,out_file):
+            self.transcripts[name].predict_effect()
 
-        fout = open(out_file,'w')
 
-        for name, fusion_transcript in self.transcripts.items():
-
-            fusion_transcript = self.transcripts[name]
-
-            fusion_transcript.fetch_transcript_cdna()
-
-            SeqIO.write(fusion_transcript.cdna,fout,"fasta")
-
-        fout.close()
-
-    def save_transcript_cds(self,out_file):
-
-        fout = open(out_file,'w')
-
-        for name, fusion_transcript in self.transcripts.items():
-
-            if not fusion_transcript.transcript1.complete or not fusion_transcript.transcript2.complete:
-                continue
-
-            fusion_transcript = self.transcripts[name]
-
-            fusion_transcript.fetch_transcript_cds()
-
-            SeqIO.write(fusion_transcript.cds,fout,"fasta")
-
-        fout.close()
-
-    def save_proteins(self,out_file):
-
-        fout = open(out_file,'w')
-
-        for name, fusion_transcript in self.transcripts.items():
-
-            if not fusion_transcript.transcript1.complete or not fusion_transcript.transcript2.complete:
-                continue
-
-            fusion_transcript = self.transcripts[name]
-
-            if fusion_transcript.cds is not None:
-                fusion_transcript.fetch_transcript_cds()
-
-            fusion_transcript.fetch_protein_seq()
-
-            SeqIO.write(fusion_transcript.protein,fout,"fasta")
-
-        fout.close()
-
-class FusionTranscript(Model):
+class FusionTranscript():
     """
     Generates the information needed for the gene fusion transctips
     """
@@ -147,8 +164,9 @@ class FusionTranscript(Model):
         self.cds=None
         self.cdna=None
         self.protein=None
+        self.molecular_weight=None
         self.domains={
-            i:{} for i,j,k in utils.PROTEIN_DOMAIN
+            i:[] for i,j,k in utils.PROTEIN_DOMAIN
         }
         self.transmembrane={
             'transmembrane_domain':{
@@ -170,11 +188,17 @@ class FusionTranscript(Model):
         self.transcript_protein_junction_5prime = 0
         self.transcript_protein_junction_3prime = 0
 
-        #types of fusions: in-frame, out-of-frame, and any combination of
-        # UTR, intron, intergenic, exonic (no known CDS),
-        # CDS(not-reliable-start-or-end), CDS(truncated), CDS(complete)
+        self.effect_5prime='exon'
+        self.effect_3prime='exon'
+        self.effect=''
 
-        self.type=''
+        self.has_start_codon_5prime=True
+        self.has_start_codon_3prime=True
+        self.has_stop_codon_5prime=True
+        self.has_stop_codon_3prime=True
+
+        self.has_coding_potential=True
+        self.predict_effect()
 
     def _annotate(self):
         """
@@ -193,7 +217,7 @@ class FusionTranscript(Model):
         domains = map(lambda x: list(x),self.db.c.fetchall())
 
         for d in domains:
-
+            d[0]=self.name
             if str(d[1])=='':
                 continue
             try:
@@ -216,6 +240,10 @@ class FusionTranscript(Model):
         domains = map(lambda x: list(x),self.db.c.fetchall())
         for d in domains:
             d[0]=self.name
+
+            if str(d[1])=='':
+                continue
+
             if self.transcript_protein_junction_3prime > int(d[3]):
                 continue
             elif self.transcript_protein_junction_5prime <= int(d[2]):
@@ -224,18 +252,20 @@ class FusionTranscript(Model):
                 d[2] = self.transcript_protein_junction_5prime
                 fusion_domains.append(d)
 
-        if self.name not in self.domains['pfam']:
-            self.domains['pfam'][self.name] = [fusion_domains]
-        else:
-            self.domains['pfam'][self.name].append(fusion_domains)
-        print self.domains['pfam'][self.name]
 
-    def fetch_protein_seq(self):
+        self.domains['pfam'] = fusion_domains
+
+    def _fetch_protein(self):
         """
         Predict the potential protain amino acid sequence
         """
 
         self.protein_names = self.transcript1.protein_id + '-' + self.transcript2.protein_id
+
+        if (len(self.cds_5prime)/3.).is_integer() and (len(self.cds_3prime)/3.).is_integer():
+            self.effect='in-frame'
+        else:
+            self.effect='out-of-frame'
 
         self.transcript_protein_junction_5prime = int(self.transcript_cds_junction_5prime/3.)
         self.transcript_protein_junction_3prime = len(self.transcript2.protein_sequence) - int(self.transcript_cds_junction_3prime/3.)
@@ -243,31 +273,27 @@ class FusionTranscript(Model):
         protein_seq = self.cds.seq.translate()
         protein_seq = protein_seq[0:protein_seq.find('*')]
 
-        self.transcript_protein_molecular_weight = SeqUtils.molecular_weight(protein_seq)/1000.
+        self.molecular_weight = SeqUtils.molecular_weight(protein_seq)/1000.
 
-        protein_seq = SeqRecord.SeqRecord(
+        self.protein = SeqRecord.SeqRecord(
             protein_seq,
             id=self.protein_names,
             name=self.protein_names,
             description="length=" + str(len(protein_seq)) + \
-                ", kD: " + str(self.transcript_protein_molecular_weight) + \
+                ", kD: " + str(self.molecular_weight) + \
                 ", transcripts: " + str(self.name) + ', ' + \
                 ", genes: " + str(self.gene_names)
         )
 
-        self.protein = protein_seq
-
         self._annotate()
 
-    def fetch_transcript_cds(self):
+    def _fetch_transcript_cds(self):
         """
         Predict the potential nucleotide sequence
         """
 
-        transcript_seq = ''
+
         transcript_junction_5prime=0
-        self.transcript_cds_junction_5prime=0
-        self.transcript_cds_junction_3prime=0
 
         #5prime transcript
 
@@ -289,7 +315,7 @@ class FusionTranscript(Model):
                 else:
                     self.transcript_cds_junction_5prime += cds[1] - self.gene5prime.junction
 
-        transcript_seq += self.transcript1.coding_sequence[0:self.transcript_cds_junction_5prime]
+        self.cds_5prime = self.transcript1.coding_sequence[0:self.transcript_cds_junction_5prime]
 
         if self.transcript2.strand=="+":
             for cds in self.transcript2.coding_sequence_position_ranges:
@@ -308,28 +334,24 @@ class FusionTranscript(Model):
                 else:
                     self.transcript_cds_junction_3prime += cds[1] - self.gene3prime.junction
 
-        transcript_seq += self.transcript1.coding_sequence[self.transcript_cds_junction_3prime::]
+        self.cds_3prime = self.transcript1.coding_sequence[self.transcript_cds_junction_3prime::]
 
-        transcript_seq = SeqRecord.SeqRecord(
-            Seq.Seq(transcript_seq,generic_dna),
+        self.cds = SeqRecord.SeqRecord(
+            Seq.Seq(self.cds_5prime + self.cds_3prime,generic_dna),
             id=self.name,
             name=self.name,
-            description="length=" + str(len(transcript_seq)) + \
+            description="length=" + str(len(self.cds_5prime + self.cds_3prime)) + \
                 ", genes: " + \
                 str(self.transcript1.gene.name) + '-' + \
                 str(self.transcript2.gene.name)
         )
 
-        self.cds = transcript_seq
-
-    def fetch_transcript_cdna(self):
+    def _fetch_transcript_cdna(self):
         """
         Predict the potential nucleotide sequence
         """
 
-        transcript_seq = ''
-        self.transcript_cdna_junction_5prime=0
-        self.transcript_cdna_junction_3prime=0
+        transcript_seq=''
 
         #5prime transcript
 
@@ -372,7 +394,7 @@ class FusionTranscript(Model):
 
         transcript_seq += self.transcript1.sequence[self.transcript_cdna_junction_3prime::]
 
-        transcript_seq = SeqRecord.SeqRecord(
+        self.cdna = SeqRecord.SeqRecord(
             Seq.Seq(transcript_seq,generic_dna),
             id=self.name,
             name=self.name,
@@ -381,4 +403,73 @@ class FusionTranscript(Model):
                 str(self.transcript2.name)
         )
 
-        self.cdna = transcript_seq
+        if self.transcript1.complete and self.transcript_cdna_junction_5prime < len(self.transcript1.five_prime_utr_sequence):
+            self.effect_5prime='5UTR'
+
+        if self.transcript2.complete and (len(self.transcript2)-self.transcript_cdna_junction_5prime) < len(self.transcript2.five_prime_utr_sequence):
+            self.effect_5prime='3UTR'
+
+    def predict_effect(self):
+        """
+        types of fusions: in-frame, out-of-frame, and any combination of
+        UTR, intron, intergenic, exonic (no known CDS),
+        CDS(not-reliable-start-or-end), CDS(truncated), CDS(complete)
+
+
+        CDS
+        exon 5' CDS
+        exon 3' CDS
+        5' utr
+        3' utr
+        intron
+        intergenic
+
+        """
+
+        #check if within exon
+
+        self._fetch_transcript_cdna()
+
+        #check if within CDS
+
+        try:
+            for cds in self.transcript1.coding_sequence_position_ranges:
+                if self.gene5prime.junction >= cds[0] and self.gene5prime.junction <= cds[1]:
+                    self.effect_5prime='CDS'
+                    break
+        except:
+            pass
+
+        try:
+            for cds in self.transcript2.coding_sequence_position_ranges:
+                if self.gene3prime.junction >= cds[0] and self.gene3prime.junction <= cds[1]:
+                    self.effect_3prime='CDS'
+                    break
+        except:
+            pass
+
+        #if within CDS, then get CDS and predict protein
+
+        if self.effect_5prime=='CDS' and self.effect_3prime=='CDS':
+            self.has_coding_potential=True
+
+        #check if the transcripts are complete, and if not check if
+        #they don't have stop and/or start codons
+
+        if not self.transcript1.contains_start_codon:
+            self.has_start_codon_5prime=False
+            self.has_coding_potential=False
+        if not self.transcript1.contains_stop_codon:
+            self.has_stop_codon_5prime=False
+            self.has_coding_potential=False
+
+        if not self.transcript2.contains_start_codon:
+            self.has_start_codon_3prime=False
+            self.has_coding_potential=False
+        if not self.transcript2.contains_stop_codon:
+            self.stop_codon_3prime=False
+            self.has_coding_potential=False
+
+        if self.has_coding_potential:
+            self._fetch_transcript_cds()
+            self._fetch_protein()
