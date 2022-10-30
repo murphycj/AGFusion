cc64769a5cd3cc31ff97e1e89aec78349090cc25
murphycj <murphy.jessj@gmail.com>
2022-10-30 16:31:52 -0400

    formate code

--- agfusion/utils.py
+++ agfusion/utils.py
@@ -1,5 +1,7 @@
-# all combinations except those that do not produce some sort of
-# chimeric protein are commented out
+"""
+All combinations except those that do not produce some sort of
+chimeric protein are commented out
+"""
 
 import itertools
 import os
@@ -98,8 +100,7 @@ JUNCTION_LOCATIONS = [
 
 CODING_COMBINATIONS = list(itertools.product(JUNCTION_LOCATIONS, JUNCTION_LOCATIONS))
 CODING_COMBINATIONS = {
-    i: {"protein_coding_potential": False, "truncated": False}
-    for i in CODING_COMBINATIONS
+    i: {"protein_coding_potential": False, "truncated": False} for i in CODING_COMBINATIONS
 }
 
 
@@ -198,11 +199,16 @@ CODING_COMBINATIONS["CDS (start)", "CDS (start)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['CDS (start)','CDS (end)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['CDS (start)','5UTR'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['CDS (start)','5UTR (end)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['CDS (start)','3UTR'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['CDS (start)','3UTR (start)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['CDS (start)','CDS (end)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['CDS (start)','5UTR'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['CDS (start)','5UTR (end)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['CDS (start)','3UTR'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['CDS (start)','3UTR (start)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 CODING_COMBINATIONS["CDS (start)", "intron"] = {
     "protein_coding_potential": True,
     "truncated": True,
@@ -236,24 +242,32 @@ CODING_COMBINATIONS["5UTR", "intron (before cds)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['5UTR','intron (after cds)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['5UTR','intron (after cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 
 # CODING_COMBINATIONS['5UTR (end)','CDS'] = {'protein_coding_potential':True,'truncated':False}
 CODING_COMBINATIONS["5UTR (end)", "CDS (start)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['5UTR (end)','CDS (end)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['5UTR (end)','5UTR'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['5UTR (end)','5UTR (end)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['5UTR (end)','3UTR'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['5UTR (end)','3UTR (start)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['5UTR (end)','intron (cds)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['5UTR (end)','CDS (end)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['5UTR (end)','5UTR'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['5UTR (end)','5UTR (end)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['5UTR (end)','3UTR'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['5UTR (end)','3UTR (start)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['5UTR (end)','intron (cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 CODING_COMBINATIONS["5UTR (end)", "intron (before cds)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['5UTR (end)','intron (after cds)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['5UTR (end)','intron (after cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 
 CODING_COMBINATIONS["3UTR", "CDS"] = {
     "protein_coding_potential": True,
@@ -345,20 +359,30 @@ CODING_COMBINATIONS["3UTR (start)", "intron (before cds)"] = {
     "truncated": False,
 }
 
-# CODING_COMBINATIONS['3UTR (end)','CDS'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['3UTR (end)','CDS (start)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['3UTR (end)','CDS (end)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['3UTR (end)','5UTR'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['3UTR (end)','5UTR (end)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['3UTR (end)','3UTR'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['3UTR (end)','3UTR (start)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['3UTR (end)','intron'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['3UTR (end)','intron (cds)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','CDS'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','CDS (start)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','CDS (end)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','5UTR'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','5UTR (end)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','3UTR'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','3UTR (start)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','intron'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','intron (cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 CODING_COMBINATIONS["3UTR (end)", "intron (before cds)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['3UTR (end)','intron (after cds)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['3UTR (end)','intron (after cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 
 # CODING_COMBINATIONS['intron','CDS'] = {'protein_coding_potential':True,'truncated':False}
 CODING_COMBINATIONS["intron", "CDS (start)"] = {
@@ -382,7 +406,13 @@ CODING_COMBINATIONS["intron", "intron (before cds)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['intron','intron (after cds)'] = {'protein_coding_potential':{'protein_coding_potential':True,'truncated':False},'truncated':False}
+# CODING_COMBINATIONS['intron','intron (after cds)'] = {
+#     'protein_coding_potential':{
+#         'protein_coding_potential':True,
+#         'truncated':False
+#     },
+#     'truncated':False
+# }
 
 CODING_COMBINATIONS["intron (cds)", "CDS"] = {
     "protein_coding_potential": True,
@@ -420,15 +450,19 @@ CODING_COMBINATIONS["intron (cds)", "intron (cds)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['intron (cds)','intron (before cds)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['intron (cds)','intron (after cds)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (cds)','intron (before cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (cds)','intron (after cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 
-# CODING_COMBINATIONS['intron (before cds)','CDS'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (before cds)','CDS'] = {
+# 'protein_coding_potential':True,'truncated':False}
 CODING_COMBINATIONS["intron (before cds)", "CDS (start)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['intron (before cds)','CDS (end)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (before cds)','CDS (end)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 CODING_COMBINATIONS["intron (before cds)", "5UTR"] = {
     "protein_coding_potential": True,
     "truncated": False,
@@ -437,15 +471,20 @@ CODING_COMBINATIONS["intron (before cds)", "5UTR (end)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['intron (before cds)','3UTR'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['intron (before cds)','3UTR (start)'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['intron (before cds)','intron'] = {'protein_coding_potential':True,'truncated':False}
-# CODING_COMBINATIONS['intron (before cds)','intron (cds)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (before cds)','3UTR'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (before cds)','3UTR (start)'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (before cds)','intron'] = {
+# 'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (before cds)','intron (cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 CODING_COMBINATIONS["intron (before cds)", "intron (before cds)"] = {
     "protein_coding_potential": True,
     "truncated": False,
 }
-# CODING_COMBINATIONS['intron (before cds)','intron (after cds)'] = {'protein_coding_potential':True,'truncated':False}
+# CODING_COMBINATIONS['intron (before cds)','intron (after cds)'] = {
+# 'protein_coding_potential':True,'truncated':False}
 
 CODING_COMBINATIONS["intron (after cds)", "CDS"] = {
     "protein_coding_potential": True,
