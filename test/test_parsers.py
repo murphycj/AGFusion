83f76f34162a9c0c987ed68b58f7b1f515816423
murphycj <charliemurphyj@gmail.com>
2023-02-06 20:42:00 -0500

    fix fusioninspector parser and add test case

--- test/test_parsers.py
+++ test/test_parsers.py
@@ -20,6 +20,9 @@ db_human95 = database.AGFusionDB(abspath(join(curdir, "agfusion.homo_sapiens.95.
 db_human95.build = "homo_sapiens_95"
 
 
+BASEDIR = "./data/FusionsFindingAlgorithms"
+
+
 class TestFusionCatcher(unittest.TestCase):
     """Test parse FusionCatcher parse."""
 
@@ -37,7 +40,7 @@ class TestFusionCatcher(unittest.TestCase):
             "C920009B18Rik_H60b",
         ]
         for fusion in parsers.parsers["fusioncatcher"](
-            "./data/FusionsFindingAlgorithms/FusionCatcher/final-list_candidate-fusion-genes.txt",
+            f"{BASEDIR}/FusionCatcher/final-list_candidate-fusion-genes.txt",
             db_mouse.logger,
         ):
             fusion = model.Fusion(
@@ -63,7 +66,7 @@ class TestArriba(unittest.TestCase):
             "BCR_ABL1",
         ]
         for fusion in parsers.parsers["arriba"](
-            "./data/FusionsFindingAlgorithms/Arriba/fusions.tsv",
+            f"{BASEDIR}/Arriba/fusions.tsv",
             db_human.logger,
         ):
             fusion = model.Fusion(
@@ -87,8 +90,7 @@ class TestSTARFusion(unittest.TestCase):
 
         all_fusions = ["ACACA_STAC2", "RPS6KB1_SNF8"]
         for fusion in parsers.parsers["starfusion"](
-            "./data/FusionsFindingAlgorithms/STARFusion/"
-            + "star-fusion.fusion_candidates.final.abridged",
+            f"{BASEDIR}/STARFusion/" + "star-fusion.fusion_candidates.final.abridged",
             db_human.logger,
         ):
             fusion = model.Fusion(
@@ -108,8 +110,7 @@ class TestSTARFusion(unittest.TestCase):
 
         all_fusions = ["ARID3B_MYCNUT", "ARID3B_MYCN", "TVP23C_CDRT4"]
         for fusion in parsers.parsers["starfusion"](
-            "./data/FusionsFindingAlgorithms/STARFusion/"
-            + "star-fusion.fusion_predictions.abridged.coding_effect.tsv",
+            f"{BASEDIR}/STARFusion/" + "star-fusion.fusion_predictions.abridged.coding_effect.tsv",
             db_human95.logger,
         ):
             fusion = model.Fusion(
@@ -133,7 +134,7 @@ class TestLongGf(unittest.TestCase):
 
         all_fusions = ["Mocos_Rprd1a", "Ubc_Ubb", "Ubc_Gm11808", "Gm21887_Gm47283"]
         for fusion in parsers.parsers["longgf"](
-            "./data/FusionsFindingAlgorithms/LongGF/fusions_mouse.log",
+            f"{BASEDIR}/LongGF/fusions_mouse.log",
             db_mouse.logger,
         ):
             fusion = model.Fusion(
@@ -153,7 +154,48 @@ class TestLongGf(unittest.TestCase):
 
         all_fusions = ["BCAS4_BCAS3", "HNRNPC_ACIN1"]
         for fusion in parsers.parsers["longgf"](
-            "./data/FusionsFindingAlgorithms/LongGF/fusions_hg38.log",
+            f"{BASEDIR}/LongGF/fusions_hg38.log",
+            db_human95.logger,
+        ):
+            fusion = model.Fusion(
+                gene5prime=fusion["gene5prime"],
+                gene5primejunction=fusion["gene5prime_junction"],
+                gene3prime=fusion["gene3prime"],
+                gene3primejunction=fusion["gene3prime_junction"],
+                db=db_human95,
+                pyensembl_data=data_human95,
+                protein_databases=["pfam"],
+                noncanonical=False,
+            )
+            assert fusion.name in all_fusions, f"{fusion.name} not in list!"
+
+
+class TestFusionInspector(unittest.TestCase):
+    """Test parse FusionInspector"""
+
+    def test_parse_human(self):
+        """Test basic parsing."""
+
+        all_fusions = ["AL627171.2_TPM3", "STAT3_AL627171.2"]
+
+        for fusion in parsers.parsers["fusioninspector"](
+            f"{BASEDIR}/FusionInspector/test.FusionInspector.fusions.abridged.txt",
+            db_human95.logger,
+        ):
+            fusion = model.Fusion(
+                gene5prime=fusion["gene5prime"],
+                gene5primejunction=fusion["gene5prime_junction"],
+                gene3prime=fusion["gene3prime"],
+                gene3primejunction=fusion["gene3prime_junction"],
+                db=db_human95,
+                pyensembl_data=data_human95,
+                protein_databases=["pfam"],
+                noncanonical=False,
+            )
+            assert fusion.name in all_fusions, f"{fusion.name} not in list!"
+
+        for fusion in parsers.parsers["fusioninspector"](
+            f"{BASEDIR}/FusionInspector/test.FusionInspector.fusions.txt",
             db_human95.logger,
         ):
             fusion = model.Fusion(
