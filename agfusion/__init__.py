c55e694b5828bebf041700d99972d23ce6a14e96
murphycj <charliemurphyj@gmail.com>
2024-09-30 00:25:15 -0400

    update version

--- agfusion/__init__.py
+++ agfusion/__init__.py
@@ -1,4 +1,5 @@
 """ __init__.py
 Initialises when agfusion module is loaded.
 """
-__version__ = "1.4.1"
+
+__version__ = "1.4.2"
--- agfusion/version.py
+++ agfusion/version.py
@@ -1,2 +1,3 @@
 """Package version."""
-__version__ = "1.4.1"
+
+__version__ = "1.4.2"
